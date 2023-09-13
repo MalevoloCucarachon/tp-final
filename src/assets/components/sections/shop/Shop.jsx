52b250ec190da88e2ba1d3fd044a64a15228cc2f
MalevoloCucarachon <matiascarini@gmail.com>
2023-09-13 20:14:33 -0300

    algunas queris mas

--- src/assets/components/sections/shop/Shop.jsx
+++ src/assets/components/sections/shop/Shop.jsx
@@ -4,12 +4,12 @@ import './Shop.css';
 
 const Shop = () => {
     const shopImg = {
-        backgroundImage : `url('https://images.unsplash.com/photo-1498579809087-ef1e558fd1da?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1740&q=80')`
+        backgroundImage: `url('https://images.unsplash.com/photo-1498579809087-ef1e558fd1da?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1740&q=80')`
     }
     return (
         <>
             <section className='shop'>
-                <div>
+                <div className='categories'>
                     <ShopCard category='Fruits'
                         imgPath='https://images.unsplash.com/photo-1623227866882-c005c26dfe41?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1665&q=80'
                         alt='strawberry'
@@ -19,12 +19,14 @@ const Shop = () => {
                         alt='pumpkins'
                     />
                 </div>
-                <div className='shop-center' style={shopImg}>
-                    <p className='vegetables'>Vegetables</p>
-                    <p className='text'>Protect the health of every home</p>
-                    <button>Shop Now</button>
+                <div className='categories'>
+                    <div className='shop-center' style={shopImg} >
+                        <p className='vegetables'>Vegetables</p>
+                        <p className='text'>Protect the health of every home</p>
+                        <button>Shop Now</button>
+                    </div>
                 </div>
-                <div>
+                <div className='categories'>
                     <ShopCard category='Juices'
                         imgPath='https://images.unsplash.com/photo-1621506289937-a8e4df240d0b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1587&q=80'
                         alt='juices'
